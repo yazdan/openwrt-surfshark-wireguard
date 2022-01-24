c6a198e58eea667e2923b4f656e7f806462f1752
Abbas Yazdanpanah <yazdanpanah.a@gmail.com>
2022-01-25 02:00:23 +0330

    Improved `gen_wg_config.sh`
    - Improved error handling, especially in curl parts
    - Add some arguments to have more control over what is going to happen
    - Moved all config variable into `read_config` function
    - Help added
    - Add some message to make the process more visible
    - make config file names interface name compatible
    - Updated `readme

--- README.md
+++ README.md
@@ -4,13 +4,41 @@ This is a simple script that generates wireguard client config files for surfsha
 
 You just need to `curl` and `jq`.
 
-I'm using this to generate configs on openwrt
+I'm using this to generate configs on openwrt.
+
+
 
 # How to use
 To use this file:
 1. copy `config.json.sample` into `config.json`
-2. replace `config.json` values with your account values. Normally user your "email" and "password" that you use on your official client on Android or iOS, not specially OpenVpn username and password
+2. replace `config.json` values with your account values. Normally user your "email" and "password" that you use on your official client on Android, iOS, or web, not specially OpenVpn username and password
 3. run `gen_wg_config.sh`
+
+## usage
+
+```shell
+Usage: gen_wg_config.sh [-f]
+  -f force register ignore checking
+  -g ignore generating profile files
+```
+
+# Caveates
+
+Please consider following caveates
+
+## Your private/public key expires
+
+As the date of writing this, each key pair expires in around 6 days so you need to rerun
+the script every now and then.
+
+I suggest to run it in crontab every day, and with `-g` parameter.
+
+## Sometimes checking if public key is peresent fails
+
+If you are not able to use the generated config files, there might be a chance that
+there is an unhandleded corner case in `wg_check_pubkey` function. I suggest you to 
+run the scring using `-f` parameter to force the script to register the key pair 
+without chekcing if it exists.
 
 # TODO
 
--- gen_wg_config.sh
+++ gen_wg_config.sh
@@ -1,11 +1,29 @@
 #!/bin/sh
-
-baseurl="https://api.surfshark.com"
-config_file="config.json"
-
 set -e
 
+parse_arg() {
+    while getopts 'fhg' opt; do
+        case "$opt" in
+            f)
+            force_register=1
+            ;;
+            g)
+            generate_conf=0
+            ;;
+            ?|h)
+            echo "Usage: $(basename $0) [-f]"
+            echo "  -f force register ignore checking"
+            echo "  -g ignore generating profile files"
+            exit 1
+            ;;
+        esac
+    done
+    shift "$(($OPTIND -1))"
+}
+
+
 read_config() {
+    config_file="config.json"
     conf_json=$(cat $config_file)
     
     config_folder="$(echo $conf_json | jq '.config_folder')"
@@ -16,19 +34,33 @@ read_config() {
 
     password="$(echo $conf_json | jq '.password')"
     password=$(eval echo $password)
+
+    token_file="${config_folder}/token.json"
+
+    baseurl="https://api.surfshark.com"
+    force_register=0
+    register=1
+    generate_conf=1
+    selected_servers_file="${config_folder}/selected_servers.json"
+    servers_file="${config_folder}/surf_servers.json"
+    wg_keys="${config_folder}/wg.json"
+    srv_conf_file_folder="${config_folder}/conf"
     
     unset conf_json
 }
 
 login () {
-    token_file="${config_folder}/token.json"
     if [ -f "$token_file" ]; then
         curl_res=$(cat $token_file)
-    else 
+    else
+        tmpfile=$(mktemp /tmp/wg-curl-res.XXXXXX)
         url="$baseurl/v1/auth/login"
         data="{\"username\":\"$username\", \"password\":\"$password\"}"
-        curl_res=$(curl -d "$data" -H 'Content-Type: application/json' -X POST $url)
-        echo $curl_res > $token_file
+        http_status=$(curl -o $tmpfile -s -w "%{http_code}" -d "$data" -H 'Content-Type: application/json' -X POST $url)
+        if [ $http_status -eq 200 ]; then
+            cp $tmpfile $token_file
+        fi
+        rm $tmpfile
     fi
     
     token=$(echo $curl_res | jq '.token')
@@ -37,25 +69,25 @@ login () {
 }
 
 get_servers() {
-    servers_file="${config_folder}/surf_servers.json"
-    echo $servers_file
     if [ -f "$servers_file" ]; then
         echo "servers list already exist"
-    else 
+    else
+        tmpfile=$(mktemp /tmp/wg-curl-res.XXXXXX)
         url="$baseurl/v4/server/clusters/generic?countryCode="
-        curl_res=$(curl -H "Authorization: Bearer $token" -H 'Content-Type: application/json' $url)
-        echo $curl_res > $servers_file
+        http_status=$(curl -o $tmpfile -s -w "%{http_code}" -H "Authorization: Bearer $token" -H 'Content-Type: application/json' $url)
+        if [ $http_status -eq 200 ]; then
+            cat $tmpfile > $servers_file
+        fi
+        rm $tmpfile
     fi
 }
 
 select_servers () {
-    selected_servers_file="${config_folder}/selected_servers.json"
     cat_res=$(cat $servers_file | jq 'select(any(.[].tags[]; . == "p2p" or . == "physical"))')
     echo $cat_res > $selected_servers_file
 }
 
 wg_gen_keys() {
-    wg_keys="${config_folder}/wg.json"
     if [ -f "$wg_keys" ]; then
         echo "wg keys already exist"
         wg_pub=$(cat $wg_keys | jq '.pub')
@@ -69,33 +101,37 @@ wg_gen_keys() {
 }
 
 wg_reg_pubkey() {
-    if [ $register -eq 1 ]; then
-        url="$baseurl/v1/account/users/public-keys"
-        data="{\"pubKey\": $wg_pub}"
-        token=$(eval echo $token)
-        curl_res=$(eval curl -H \"Authorization: Bearer $token\" -H \"Content-Type: application/json\"  -d \'$data\' -X POST $url)
-    else
-        echo "No need to register pubkey"
-    fi
-}
-
-wg_check_pubkey() {
-    url="$baseurl/v1/account/users/public-keys/validate"
+    url="$baseurl/v1/account/users/public-keys"
     data="{\"pubKey\": $wg_pub}"
     token=$(eval echo $token)
     curl_res=$(eval curl -H \"Authorization: Bearer $token\" -H \"Content-Type: application/json\"  -d \'$data\' -X POST $url)
-    expire_date=$(echo $curl_res | jq '.expiresAt')
-    expire_date=$(eval echo $expire_date)
-    now=$(date -Iseconds --utc)
-    register=1
-    if [ "${now}" '<' "${expire_date}" ]
-    then
-        register=0
+}
+
+wg_check_pubkey() {
+    tmpfile=$(mktemp /tmp/wg-curl-res.XXXXXX)
+    url="$baseurl/v1/account/users/public-keys/validate"
+    data="{\"pubKey\": $wg_pub}"
+    token=$(eval echo $token)
+    http_status=$(eval curl -o $tmpfile -s -w "%{http_code}" -H \"Authorization: Bearer $token\" -H \"Content-Type: application/json\"  -d \'$data\' -X POST $url)
+    if [ $http_status -eq 200 ]; then
+        curl_res=$(cat $tmpfile)
+        expire_date=$(echo $curl_res | jq '.expiresAt')
+        expire_date=$(eval echo $expire_date)
+        now=$(date -Iseconds --utc)
+        if [ "${now}" '<' "${expire_date}" ];then
+            register=0
+        fi
+    elif [ $http_status -eq 401 ]; then
+        rm -f $token_file
+        echo "Unauthorized. Please run again"
+        rm $tmpfile
+        exit 1
     fi
+    rm $tmpfile
 }
 
 gen_client_confs() {
-    srv_conf_file_folder="${config_folder}/conf"
+    postf=".surfshark.com"
     mkdir -p $srv_conf_file_folder
     server_hosts=$(echo "${cat_res}" | jq -c '.[] | [.connectionName, .pubKey]')
     for row in $server_hosts; do
@@ -107,7 +143,7 @@ gen_client_confs() {
 
         echo "generating config for $srv_host"
 
-        srv_conf_file="${srv_conf_file_folder}/$srv_host.conf"
+        srv_conf_file="${srv_conf_file_folder}/${srv_host%$postf}.conf"
 
         srv_conf="[Interface]\nPrivateKey=$wg_prv\nAddress=10.14.0.2/8\nMTU=1350\n\n[Peer]\nPublicKey=o07k/2dsaQkLLSR0dCI/FUd3FLik/F/HBBcOGUkNQGo=\nAllowedIPs=172.16.0.36/32\nEndpoint=wgs.prod.surfshark.com:51820\nPersistentKeepalive=25\n\n[Peer]\nPublicKey=$srv_pub\nAllowedIPs=0.0.0.0/0\nEndpoint=$srv_host:51820\nPersistentKeepalive=25\n"
 
@@ -122,10 +158,32 @@ gen_client_confs() {
 }
 
 read_config
+parse_arg "$@"
+
+echo "Loggin in if needed ..."
 login
+echo "Getting the list of servers ..."
 get_servers
+echo "Selecting servers ..."
 select_servers
+
+echo "Generating keys ..."
 wg_gen_keys
-wg_check_pubkey
-wg_reg_pubkey
-gen_client_confs
+
+if [ $force_register -eq 0 ]; then
+    echo "Checking pubkey ..."
+    wg_check_pubkey
+fi
+
+if [ $register -eq 1 ]; then
+    echo "Registring pubkey ..."
+    wg_reg_pubkey
+else
+    echo "No need to register pubkey"
+fi
+
+if [ $generate_conf -eq 1 ]; then
+echo "Generating profiles..."
+    gen_client_confs
+fi
+echo "Done!"
